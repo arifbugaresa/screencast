216ce213ef2bc378debb5b1b708e9acf32ad8f60
butga <arifyuniartofajarbugaresa@gmail.com>
2021-01-25 22:20:19 +0700

    Setup Layout Dashboard

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,28 @@
+<div class="w-1/5  bg-gray-800 min-h-screen px-4 py-6">
+    <div class="mb-8">
+        <header class="font-medium py-2 text-gray-400 uppercase text-sm">
+            Main
+        </header>
+        <a href="#" class="block text-gray-200 px-2 py-2">Dashboard</a>
+    </div>
+    <div class="mb-8">
+        <header class="font-medium py-2 text-gray-400 uppercase text-sm">
+            Playlist
+        </header>
+        <a href="#" class="block text-gray-200 px-2 py-2">Create</a>
+        <a href="#" class="block text-gray-200 px-2 py-2">Table</a>
+    </div>
+    <div class="mb-8">
+        <header class="font-medium py-2 text-gray-400 uppercase text-sm">
+            Category
+        </header>
+        <a href="#" class="block text-gray-200 px-2 py-2">Create</a>
+        <a href="#" class="block text-gray-200 px-2 py-2">Table</a>
+    </div>
+    <div class="mb-8">
+        <header class="font-medium py-2 text-gray-400 uppercase text-sm">
+            Users
+        </header>
+        <a href="#" class="block text-gray-200 px-2 py-2">Table</a>
+    </div>
+</div>
